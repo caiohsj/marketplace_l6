4031ed452e2a5344de7fd8f2f6604230499d4f43
Caio Henrique <caiocoxim10@gmail.com>
2020-06-30 14:03:40 -0300

    importando jquery ajax e gerando token do cartão de crédito

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -10,8 +10,8 @@
                     <div class="col-md-9">
                         <div class="form-group">
                             <label for="card_number">Número do Cartão</label> <span class="brand"></span>
-                            <input type="text" name="card_number" id="card_number" class="form-control"/>
-                            
+                            <input type="text" name="cardNumber" id="card_number" class="form-control"/>
+                            <input type="hidden" name="cardBrand"/>
                         </div>
                     </div>
                 </div>
@@ -20,14 +20,14 @@
                     <div class="col-md-4">
                         <div class="form-group">
                             <label for="card_month">Mês de Expiração</label>
-                            <input type="text" name="card_month" id="card_month" class="form-control"/>
+                            <input type="text" name="cardMonth" id="card_month" class="form-control"/>
                         </div>
                     </div>
 
                     <div class="col-md-5">
                         <div class="form-group">
                             <label for="card_year">Ano de Expiração</label>
-                            <input type="text" name="card_year" id="card_year" class="form-control"/>
+                            <input type="text" name="cardYear" id="card_year" class="form-control"/>
                         </div>
                     </div>
                 </div>
@@ -36,11 +36,13 @@
                     <div class="col-md-9">
                         <div class="form-group">
                             <label for="card_cvv">CVV</label>
-                            <input type="text" name="card_cvv" id="card_cvv" class="form-control"/>
+                            <input type="text" name="cardCvv" id="card_cvv" class="form-control"/>
                         </div>
                     </div>
                     <div class="col-md-12 installments"></div>
                 </div>
+
+                <button type="button" class="btn btn-success button-checkout">Comprar</button>
             </form>
         </div>
     </div>
@@ -48,15 +50,17 @@
 
 @section('scripts')
     <script src="https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js"></script>
+    <script src="{{asset('js/jquery.ajax.js')}}"></script>
     <script>
         const sessionId = '{{session()->get('pagseguroSessionCode')}}';
         PagSeguroDirectPayment.setSessionId(sessionId);
     </script>
 
     <script>
-        let cardNumber = document.querySelector('input[name=card_number]');
+        let cardNumber = document.querySelector('input[name=cardNumber]');
         let spanBrand = document.querySelector('span.brand');
         let divInstallments = document.querySelector('div.installments');
+        let buttonCheckout = document.querySelector('button.button-checkout');
         cardNumber.addEventListener('keyup', function () {
             if (cardNumber.value.length >= 6) {
                 PagSeguroDirectPayment.getBrand({
@@ -65,6 +69,7 @@
                         let imgBrand = `<img src="https://stc.pagseguro.uol.com.br/public/img/payment-methods-flags/68x30/${response.brand.name}.png" />`;
                         spanBrand.innerHTML = imgBrand;
                         getInstallments(100,response.brand.name);
+                        document.querySelector('input[name=cardBrand]').value = response.brand.name;
                     },
                     error: function (response) {
                         console.log(response);
@@ -85,6 +90,26 @@
 
                 }
             });
+        }
+
+        buttonCheckout.addEventListener('click', function() {
+            PagSeguroDirectPayment.createCardToken({
+                cardNumber: document.querySelector('input[name=cardNumber]').value,
+                brand: document.querySelector('input[name=cardBrand]').value,
+                cvv: document.querySelector('input[name=cardCvv]').value,
+                expirationMonth: document.querySelector('input[name=cardMonth]').value,
+                expirationYear: document.querySelector('input[name=cardYear]').value,
+                success: function(response) {
+                    console.log(response)
+                },
+                error: function(response) {
+                    console.log(response)
+                }
+            });
+        });
+
+        function proccessCheckout() {
+
         }
 
         function drawSelectInstallments(installments) {
